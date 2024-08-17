b0e5b9761f9e99215cd2ec42f97756a1bc40bc09
Robin_ <3045351695@qq.com>
2024-08-17 12:17:49 +0800

    "结算-路由配置和基础渲染"

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Category from '@/views/Category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
+import Checkout from '@/views/Checkout/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -38,6 +39,11 @@ const router = createRouter({
           path:'cartlist',
           name:'cartlist',
           component:CartList
+        },
+        {
+          path:'checkout',
+          name:'checkout',
+          component:Checkout
         }
       ]
     },
