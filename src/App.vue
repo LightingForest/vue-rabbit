b5ed812c6a8f66b578948b77f27e534197488796
Robin_ <3045351695@qq.com>
2024-08-17 20:01:43 +0800

    "layout静态结构搭建"

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,9 @@
 <template>
 <!--  一级路由出口-->
   <RouterView/>
-  <div class="test">
-    test class
-  </div>
+
 </template>
 
 <style scoped lang="scss">
-.test{
-  color: $priceColor;
-}
+
 </style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,15 +1,12 @@
 <script setup>
-
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
 </script>
 
 <template>
-  <div>
-    我是主页
-    <RouterView/>
-  </div>
-
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
-
-<style scoped>
-
-</style>
