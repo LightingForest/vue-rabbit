8ade8bcdc6e4646df4a36b15b91a5d24dc3b6d99
Robin_ <3045351695@qq.com>
2024-08-18 20:55:03 +0800

    "pinia优化重复请求"

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,16 +1,8 @@
 <script setup>
-import {getCategoryAPI} from "@/apis/testApi.js";
-import {onMounted, ref} from "vue";
-const categoryList=ref([])
-const getCategory=async ()=>{
-  const res=await getCategoryAPI()
-  console.log(res)
-  categoryList.value=res.result
-}
+import {useCategoryStore} from "@/stores/category.js";
 
-onMounted(()=>{
-  getCategory()
-})
+const categoryStore=useCategoryStore()
+
 </script>
 
 <template>
@@ -20,7 +12,7 @@ onMounted(()=>{
         <RouterLink to="/">小兔鲜</RouterLink>
       </h1>
       <ul class="app-header-nav">
-        <li class="home" v-for="item in categoryList" :key="item.id">
+        <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
           <RouterLink to="/">{{item.name}}</RouterLink>
         </li>
 
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -3,6 +3,12 @@ import LayoutNav from './components/LayoutNav.vue'
 import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 import LayoutFixed from "@/views/Layout/components/LayoutFixed.vue";
+import {useCategoryStore} from "@/stores/category.js";
+import {onMounted} from "vue";
+
+//触发获取导航列表的action
+const categoryStore=useCategoryStore()
+onMounted(()=>categoryStore.getCategory())
 </script>
 
 <template>
