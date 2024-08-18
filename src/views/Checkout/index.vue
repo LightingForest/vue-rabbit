12b3aeb22b3c72ca7d3c12ef74312d2f0b3ea368
Robin_ <3045351695@qq.com>
2024-08-18 19:47:57 +0800

    "结算-地址切换交互实现"

--- src/views/Checkout/index.vue
+++ src/views/Checkout/index.vue
@@ -15,6 +15,18 @@ const getCheckInfo=async ()=>{
 //控制弹窗打开
 const showDialog=ref(false)
 
+//切换地址
+const activeAddress=ref({})
+const switchAddress=(item)=>{
+  activeAddress.value=item
+}
+const confirm=()=>{
+  defaultAddress.value=activeAddress.value
+  showDialog.value=false
+  activeAddress.value={}
+}
+
+
 onMounted(getCheckInfo)
 </script>
 
@@ -116,9 +128,9 @@ onMounted(getCheckInfo)
     </div>
   </div>
   <!-- 切换地址 -->
-  <el-dialog v-model="showDialog" title="切换收货地址" width="30%" center>
+  <el-dialog v-model="showDialog"  title="切换收货地址" width="30%" center>
     <div class="addressWrapper">
-      <div class="text item" v-for="item in checkInfo.userAddresses"  :key="item.id">
+      <div class="text item" :class="{active:activeAddress.id===item.id}" @click="switchAddress(item)" v-for="item in checkInfo.userAddresses"  :key="item.id">
         <ul>
           <li><span>收<i />货<i />人：</span>{{ item.receiver }} </li>
           <li><span>联系方式：</span>{{ item.contact }}</li>
@@ -129,7 +141,7 @@ onMounted(getCheckInfo)
     <template #footer>
     <span class="dialog-footer">
       <el-button>取消</el-button>
-      <el-button type="primary">确定</el-button>
+      <el-button type="primary" @click="confirm">确定</el-button>
     </span>
     </template>
   </el-dialog>
