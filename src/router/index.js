af4514a3a6b411e91cf1e1bacac8aa3f887000e3
Robin_ <3045351695@qq.com>
2024-08-18 20:46:58 +0800

    "支付-支付结果展示"

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
 import Checkout from '@/views/Checkout/index.vue'
 import Pay from '@/views/Pay/index.vue'
+import PayBack from "@/views/Login/PayBack.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -51,6 +52,11 @@ const router = createRouter({
           path:'pay',
           name:'pay',
           component:Pay
+        },
+        {
+          path:'/paycallback',
+          name:'paycallback',
+          component:PayBack
         }
 
       ]
