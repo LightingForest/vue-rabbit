3aadc1f6cdb09f77768de2b0b8c58fd155eeb466
Robin_ <3045351695@qq.com>
2024-08-18 21:14:16 +0800

    "支付-封装倒计时函数"

--- src/composables/useCountDown.js
+++ src/composables/useCountDown.js
@@ -7,9 +7,7 @@ export const useCountDown=()=>{
     //1.响应式数据
     const time=ref(0)
     //格式化时间 xx分xx秒
-    const formatTime= computed(()=>{
-        dayjs.unix(time.value).format('mm分ss秒')
-    })
+    const formatTime = computed(() => dayjs.unix(time.value).format('mm分ss秒'))
     //2.开启倒计时的函数
     const start=(currentTime)=>{
         //开始倒计时逻辑
