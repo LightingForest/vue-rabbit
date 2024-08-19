12e54739c22715516078a559bdc277733afdd03e
Robin_ <3045351695@qq.com>
2024-08-19 20:11:32 +0800

    "一级路由配置"

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const router = createRouter({
           component:Home
         },
         {
-          path:'category',
+          path:'category/:id',
           component:Category
         }
       ]
