f11a74aaaffcbdaefe53a6fdb70a1905715d7c52
Robin_ <3045351695@qq.com>
2024-08-19 22:21:07 +0800

    "二级分类页-整体认识和路由配置"

--- src/router/index.js
+++ src/router/index.js
@@ -3,25 +3,35 @@ import Login from '@/views/Login/index.vue'
 import Layout from '@/views/Layout/index.vue'
 import Home from '@/views/Home/index.vue'
 import Category from '@/views/Category/index.vue'
+import SubCategory from '@/views/SubCategory/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path:'/',
+      name:'layout',
       component:Layout,
       children:[
         {
           path:'',
+          name:'home',
           component:Home
         },
         {
           path:'category/:id',
+          name:'category',
           component:Category
+        },
+        {
+          path:'category/sub/:id',
+          name:'subcategory',
+          component:SubCategory
         }
       ]
     },
     {
       path:'/login',
+      name:'login',
       component:Login
     }
   ]
