c9588ea54dd783e3b664fbd925f2f8e459e6ff66
Robin_ <3045351695@qq.com>
2024-08-21 17:27:01 +0800

    "登录页-表单校验-统一校验"

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -32,6 +32,19 @@ const rules={
     }
   ]
 }
+
+//获取form实例进行统一校验
+const formRef=ref(null)
+const doLogin=()=>{
+  //调用实例方法
+  formRef.value.validate((valid)=>{
+    //valid 所有表单通过校验 才为true
+    console.log(valid)
+    if(valid){
+      //TODO LOGIN
+    }
+  })
+}
 </script>
 
 
@@ -56,7 +69,7 @@ const rules={
         </nav>
         <div class="account-box">
           <div  class="form">
-            <el-form :model="form" :rules="rules" label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="form" :rules="rules" label-position="right" label-width="60px"
                      status-icon>
               <el-form-item prop="account"  label="账户">
                 <el-input v-model="form.account"/>
@@ -69,7 +82,7 @@ const rules={
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
