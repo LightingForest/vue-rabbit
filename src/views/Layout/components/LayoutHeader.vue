d524fbdd3d2e7d0746a4fe58b5174ca2c659d50f
Robin_ <3045351695@qq.com>
2024-08-22 17:30:26 +0800

    "购物车-头部购物车列表渲染"

--- src/views/Layout/components/LayoutHeader.vue
+++ src/views/Layout/components/LayoutHeader.vue
@@ -1,5 +1,6 @@
 <script setup>
 import {useCategoryStore} from "@/stores/category.js";
+import HeaderCart from "@/views/Layout/components/HeaderCart.vue";
 
 const categoryStore=useCategoryStore()
 
@@ -25,7 +26,7 @@ const categoryStore=useCategoryStore()
         <input type="text" placeholder="搜一搜">
       </div>
       <!-- 头部购物车 -->
-
+      <HeaderCart/>
     </div>
   </header>
 </template>
