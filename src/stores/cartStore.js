b4f4a73d4bb702a0e87f0e8a20801f75eb3cff4b
Robin_ <3045351695@qq.com>
2024-08-22 17:50:49 +0800

    "购物车-头部购物车统计计算"

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,5 +1,5 @@
 import {defineStore} from "pinia";
-import {ref} from "vue";
+import {computed, ref} from "vue";
 
 export const useCartStore= defineStore('cart',()=>{
     //定义state
@@ -23,10 +23,18 @@ export const useCartStore= defineStore('cart',()=>{
         const idx = cartList.value.findIndex((item)=>skuId===item.skuId)
         cartList.value.splice(idx,1)
     }
+    //计算属性
+    //总的数量
+    const  allCount= computed(()=>cartList.value.reduce((a,c)=>a+c.count,0))
+    //总价
+    const  allPrice= computed(()=>cartList.value.reduce((a,c)=>a+c.count*c.price,0))
+
     return{
         cartList,
         addCart,
-        delCart
+        delCart,
+        allCount,
+        allPrice
     }
 },{
     persist:true
